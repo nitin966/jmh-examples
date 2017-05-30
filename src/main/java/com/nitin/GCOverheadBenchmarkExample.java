499f1a159bdc154ae7130089d64595a7dc628ff7
Nitin Kesarwani <nitin966@gmail.com>
2017-05-29 23:16:58 -0700

    Add benchmarking code to compare GC overhead between HashMap and TreeMap in Java.
    Signed-off-by: Nitin Kesarwani <nitin966@gmail.com>

--- src/main/java/com/nitin/GCOverheadBenchmarkExample.java
+++ src/main/java/com/nitin/GCOverheadBenchmarkExample.java
@@ -1,40 +1,7 @@
-/*
- * Copyright (c) 2014, Oracle America, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *  * Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- *  * Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- *  * Neither the name of Oracle nor the names of its contributors may be used
- *    to endorse or promote products derived from this software without
- *    specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
- * THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package com.nitin;
 
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.Blackhole;
-import org.openjdk.jmh.profile.ClassloaderProfiler;
-import org.openjdk.jmh.profile.LinuxPerfProfiler;
 import org.openjdk.jmh.profile.*;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
@@ -67,6 +34,16 @@ public class GCOverheadBenchmarkExample {
         private int begin;
         private int end;
 
+        public static void main(String[] args) throws RunnerException {
+            Options opt = new OptionsBuilder()
+                    .include(GCOverheadBenchmarkExample.Maps.class.getSimpleName())
+                    //.addProfiler(StackProfiler.class)
+                    .addProfiler(WinPerfAsmProfiler.class)
+                    .build();
+
+            new Runner(opt).run();
+        }
+
         @Setup
         public void setup() {
             switch (type) {
@@ -87,13 +64,6 @@ public class GCOverheadBenchmarkExample {
             }
         }
 
-        @Benchmark
-        public void test(Blackhole bh) {
-            for (int i = begin; i < end; i++) {
-                bh.consume(map.get(i));
-            }
-        }
-
         /*
          * ============================== HOW TO RUN THIS TEST: ====================================
          *
@@ -109,14 +79,11 @@ public class GCOverheadBenchmarkExample {
          *      http://openjdk.java.net/projects/code-tools/jmh/)
          */
 
-        public static void main(String[] args) throws RunnerException {
-            Options opt = new OptionsBuilder()
-                    .include(GCOverheadBenchmarkExample.Maps.class.getSimpleName())
-                    //.addProfiler(StackProfiler.class)
-                    .addProfiler(WinPerfAsmProfiler.class)
-                    .build();
-
-            new Runner(opt).run();
+        @Benchmark
+        public void test(Blackhole bh) {
+            for (int i = begin; i < end; i++) {
+                bh.consume(map.get(i));
+            }
         }
     }
 }
